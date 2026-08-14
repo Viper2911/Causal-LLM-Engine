f3a2f0e0bccccb779c3e929bd35e588f2e604160
Aadil Hasan <aadilhasan29.5@gmail.com>
2026-08-14 12:47:05 +0530

    Causal Analysis Run function pushed

--- math_engine/causal_model.py
+++ math_engine/causal_model.py
@@ -50,3 +50,53 @@ def discover_stronger_causes(df:pd.DataFrame, treatment:str, outcome:str, confou
     except Exception as e:
         print(f"Discovery Enginer Warning: {e}")
         return []
+def run_causal_analysis(df:pd.DataFrame, treatment:str, outcome:str, confounders:list)->dict:
+    """Executes DoWhy pipeline + Causal Discovery."""
+    if df.empty:
+        return {"status":"error","message":"DataFrame is empty."}
+    if treatment not in df.columns or outcome not in df.columns:
+        return {"status":"error","message":"Treatment or Outcome missing from dataset."}
+    df_clean=preprocess_data(df,treatment,outcome)
+    valid_confounders=[c for c in confounders if c in df_clean.columns]
+
+    alternative_causes=discover_stronger_causes(df_clean,treatment,outcome,valid_confounders)
+    try:
+        model=CausalModel(
+            data=df_clean,
+            treatment=treatment,
+            outcome=outcome,
+            common_causes=valid_confounders
+        )
+        identified_estimand=model.identify_effect(proceed_when_unidentifiable=True)
+        estimate=model.estimate_effect(
+            identified_estimand,
+            method_name="backdoor.linear_regression",
+            test_significance=True
+        )
+        placebo=model.refute_estimate(identified_estimand,estimate,method_name="placebo_treatment_refuter",num_simulations=10)
+        random_cause=model.refute_estimate(identified_estimand,estimate,method_name="random_common_cause",num_simulations=10)
+        
+        return{
+            "status":"success",
+            "ate":round(float(estimate.value),4),
+            "alternative_causes":alternative_causes,
+            "estimate_summary":str(estimate),
+            "placebo_summary":str(placebo),
+            "random_cause_summary":str(random_cause)
+        }
+    except Exception as e:
+        return {"status":"error","message":f"DoWhy calculation failed: {str(e)}"}
+
+if __name__ == "__main__":
+    print("Testing Causal Engine with Discovery...")
+    dummy_data=pd.DataFrame({
+        "delayed_shipping":[0,1,0,1,0,1,0,1,0,1],
+        "review_score":[5,4,5,3,4,3,5,2,4,2],
+        "freight_value":[10,80,15,90,12,85,10,95,14,88]
+    })
+    result=run_causal_analysis(dummy_data,"delayed_shipping","review_score",["freight_value"])
+    print(f"ATE: {result['ate']}")
+    if result['alternative_causes']:
+        print("\n DISCOVERY ALERT: The engine found a stronger cause!")
+        for alt in result['alternative_causes']:
+            print(f"- '{alt['variable']}' has an impact score of {alt['confounder_impact']}, beating the treatment's score of {alt['treatment_impact']}.")
